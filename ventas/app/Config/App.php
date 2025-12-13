2cb7b250db7d1f192f932346f741d188cbdc0c5b
RodrigoApaza7 <rodrigoapazajh123@gmail.com>
2025-12-13 10:26:54 -0500

    ip corregida

--- ventas/app/Config/App.php
+++ ventas/app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://TU_IP/ventas/public/';
+    public string $baseURL = 'http://3.16.227.140/ventas/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
