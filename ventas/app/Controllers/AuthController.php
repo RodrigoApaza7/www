9dce6457dc7d051cb70576f4d02b471d015d5ed5
RodrigoApaza7 <rodrigoapazajh123@gmail.com>
2025-12-13 11:06:53 -0500

    a

--- ventas/app/Controllers/AuthController.php
+++ ventas/app/Controllers/AuthController.php
@@ -41,6 +41,6 @@ class AuthController extends BaseController
     public function logout()
     {
         session()->destroy();
-        return redirect()->to('/login');
+        return redirect()->to('login');
     }
 }
