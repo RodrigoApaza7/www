d30a98f8260d8db8e82127477128baf29a94ee1d
RodrigoApaza7 <rodrigoapazajh123@gmail.com>
2025-12-14 16:13:51 -0500

    a

--- ventas/app/Controllers/DashboardController.php
+++ ventas/app/Controllers/DashboardController.php
@@ -6,6 +6,9 @@ class DashboardController extends BaseController
 {
     public function index()
     {
+        var_dump(session()->get());
+        die();
+
         if (!session()->get('logged_in')) {
             return redirect()->to('login');
         }
