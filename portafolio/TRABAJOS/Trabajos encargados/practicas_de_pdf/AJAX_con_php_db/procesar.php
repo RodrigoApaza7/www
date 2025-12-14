053650db31409f4032954fd64bda226d0201a73e
RodrigoApaza7 <rodrigoapazajh123@gmail.com>
2025-12-14 15:02:42 -0500

    a2

--- portafolio/TRABAJOS/Trabajos encargados/practicas_de_pdf/AJAX_con_php_db/procesar.php
+++ portafolio/TRABAJOS/Trabajos encargados/practicas_de_pdf/AJAX_con_php_db/procesar.php
@@ -1,23 +1,26 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-var_dump($_POST);
-exit;
+
 $conexion = new mysqli(
     "3.16.227.140",
     "ventas_user",
     "ventas123",
     "ajax_db"
 );
+
 if ($conexion->connect_error) {
     die($conexion->connect_error);
 }
+
 $nombres   = $_POST['nombres'];
 $apellidos = $_POST['apellidos'];
 
-$sql = "INSERT INTO personas(nombres, apellidos)
+$sql = "INSERT INTO personas (nombres, apellidos)
         VALUES ('$nombres', '$apellidos')";
 
-$conexion->query($sql);
+if (!$conexion->query($sql)) {
+    die($conexion->error);
+}
 
 echo "Guardado correctamente";
