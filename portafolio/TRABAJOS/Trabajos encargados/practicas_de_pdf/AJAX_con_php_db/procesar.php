27c8f7e466e2f7ce4a3956296e7483fc72038ff8
RodrigoApaza7 <rodrigoapazajh123@gmail.com>
2025-12-14 14:53:57 -0500

    a

--- portafolio/TRABAJOS/Trabajos encargados/practicas_de_pdf/AJAX_con_php_db/procesar.php
+++ portafolio/TRABAJOS/Trabajos encargados/practicas_de_pdf/AJAX_con_php_db/procesar.php
@@ -3,12 +3,13 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $conexion = new mysqli(
     "3.16.227.140",
-    "ventas_user"
+    "ventas_user",
     "ventas123",
     "ajax_db"
 );
-if ($conexion->connect_error) die("Error BD");
-
+if ($conexion->connect_error) {
+    die($conexion->connect_error);
+}
 $nombres   = $_POST['nombres'];
 $apellidos = $_POST['apellidos'];
 
