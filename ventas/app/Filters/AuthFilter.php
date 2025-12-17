c3673f5df3bd1959d30e8398bb2d7fd4438b3fa3
RodrigoApaza7 <rodrigoapazajh123@gmail.com>
2025-12-17 13:05:55 -0500

    a

--- ventas/app/Filters/AuthFilter.php
+++ ventas/app/Filters/AuthFilter.php
@@ -5,35 +5,28 @@ namespace App\Filters;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Filters\FilterInterface;
+use Config\Services;
 
 class AuthFilter implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
-{
-    log_message('error', 'DEBUG FILTER: ' . json_encode([
-        'logged_in'   => session()->get('logged_in'),
-        'usuario_rol' => session()->get('usuario_rol'),
-        'arguments'   => $arguments,
-    ]));
+    {
+        $session = Services::session();
 
-    if (!session()->get('logged_in')) {
-        return redirect()->to('/login');
-    }
+        if (!$session->get('logged_in')) {
+            return redirect()->to('/login');
+        }
 
-    if (!empty($arguments)) {
-        $rolUsuario = session()->get('usuario_rol');
+        if (!empty($arguments)) {
+            $rolUsuario = $session->get('usuario_rol');
 
-        if (!$rolUsuario || !in_array($rolUsuario, $arguments)) {
-            return redirect()->to(site_url('dashboard'));
+            if (!$rolUsuario || !in_array($rolUsuario, $arguments)) {
+                return redirect()->to(site_url('dashboard'));
+            }
         }
     }
-}
-
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
-        // No se usa
     }
-    public $filters = [];
-
 }
