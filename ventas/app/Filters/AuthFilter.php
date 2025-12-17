5425bbab3fa7354659bb351ce802f62a91230666
RodrigoApaza7 <rodrigoapazajh123@gmail.com>
2025-12-17 13:01:00 -0500

    prueba 3

--- ventas/app/Filters/AuthFilter.php
+++ ventas/app/Filters/AuthFilter.php
@@ -9,27 +9,26 @@ use CodeIgniter\Filters\FilterInterface;
 class AuthFilter implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
-    {
-        dd([
-            'logged_in' => session()->get('logged_in'),
-            'usuario_rol' => session()->get('usuario_rol'),
-            'arguments' => $arguments,
-        ]);
+{
+    log_message('error', 'DEBUG FILTER: ' . json_encode([
+        'logged_in'   => session()->get('logged_in'),
+        'usuario_rol' => session()->get('usuario_rol'),
+        'arguments'   => $arguments,
+    ]));
 
-        //Verificar login
-        if (!session()->get('logged_in')) {
-            return redirect()->to('/login');
-        }
+    if (!session()->get('logged_in')) {
+        return redirect()->to('/login');
+    }
 
-        //Verificar roles (si se pasan)
-        if (!empty($arguments)) {
-            $rolUsuario = session()->get('usuario_rol');
+    if (!empty($arguments)) {
+        $rolUsuario = session()->get('usuario_rol');
 
-            if (!$rolUsuario || !in_array($rolUsuario, $arguments)) {
-                return redirect()->to(site_url('dashboard'));
-            }
+        if (!$rolUsuario || !in_array($rolUsuario, $arguments)) {
+            return redirect()->to(site_url('dashboard'));
         }
     }
+}
+
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
