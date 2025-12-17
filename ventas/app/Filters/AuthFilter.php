e114d402c14dbf0c1322941ac67be9372c5e1549
RodrigoApaza7 <rodrigoapazajh123@gmail.com>
2025-12-17 12:51:00 -0500

    a

--- ventas/app/Filters/AuthFilter.php
+++ ventas/app/Filters/AuthFilter.php
@@ -9,13 +9,21 @@ use CodeIgniter\Filters\FilterInterface;
 class AuthFilter implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
-{
-    dd([
-        'logged_in' => session()->get('logged_in'),
-        'usuario_rol' => session()->get('usuario_rol'),
-        'arguments' => $arguments,
-    ]);
+    {
+        //Verificar login
+        if (!session()->get('logged_in')) {
+            return redirect()->to('/login');
+        }
 
+        //Verificar roles (si se pasan)
+        if (!empty($arguments)) {
+            $rolUsuario = session()->get('usuario_rol');
+
+            if (!$rolUsuario || !in_array($rolUsuario, $arguments)) {
+                return redirect()->to(site_url('dashboard'));
+            }
+        }
+    }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
