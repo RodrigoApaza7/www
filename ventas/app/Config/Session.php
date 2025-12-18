8dd7415769982519eec70138dbd26a995b2cdf36
RodrigoApaza7 <rodrigoapazajh123@gmail.com>
2025-12-18 12:21:24 -0500

    a

--- ventas/app/Config/Session.php
+++ ventas/app/Config/Session.php
@@ -31,6 +31,8 @@ class Session extends BaseConfig
      * The session cookie name, must contain only [0-9a-z_-] characters
      */
     public string $cookieName = 'ci_session';
+    public bool $cookieSecure = false;
+    public string $cookieSameSite = 'Lax';
 
     /**
      * --------------------------------------------------------------------------
