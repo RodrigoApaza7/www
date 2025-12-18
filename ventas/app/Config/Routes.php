290e814a4618b70db544ba3fb3679e0c64edcc3e
RodrigoApaza7 <rodrigoapazajh123@gmail.com>
2025-12-18 14:35:50 -0500

    routes com

--- ventas/app/Config/Routes.php
+++ ventas/app/Config/Routes.php
@@ -81,9 +81,9 @@ $routes->group('', ['filter' => 'auth:admin,vendedor'], function ($routes) {
     // =======================
     // Ventas
     // =======================
-    $routes->get('ventas', 'VentasController::index');
-    $routes->post('ventas/agregar-producto', 'VentasController::agregarProducto');
-    $routes->post('ventas/finalizar', 'VentasController::finalizar');
+    $routes->get('ventas_tienda', 'VentasController::index');
+    $routes->post('ventas_tienda/agregar-producto', 'VentasController::agregarProducto');
+    $routes->post('ventas_tienda/finalizar', 'VentasController::finalizar');
 
     // Clientes
     $routes->get('clientes', 'ClientesController::index');
